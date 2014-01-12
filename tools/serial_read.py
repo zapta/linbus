aa17a452cfc56eafe1749b98c4557863904d227a
Zapta <tal@tal-macbookair.roam.corp.google.com>
2014-01-12 00:14:16 -0800

    Extended the serial port reader to parse frame and show diffs in real time.

--- tools/serial_read.py
+++ tools/serial_read.py
@@ -10,56 +10,97 @@
 
 import getopt
 import os
+import re
 import select
 import sys
 import termios
 import time
 
+# TODO: make these flags
+kPrintRawLines = False
+kPrintFrameDiffs = True
+
 # TODO: make these command line args.
-flag_speed = 115200
-flag_port = "/dev/cu.usbserial-A600dOYP"
+FLAG_speed = 115200
+FLAG_port = "/dev/cu.usbserial-A600dOYP"
 
 # For getopt parsing.
-flag_names = [
+kFlagNames = [
   "speed=",
   "port="
 ]
 
-usage = [
-  "serial_read.py [--port=<port>] [--speed=<speed>]"
+kUsage = [
+  "serial_read.py [option ...]",
+  "Options:",
+  "--port=<port>",
+  "--speed=<speed>"
 ]
+
+# NOTE: excluding frames with ERR suffix.
+kFrameRegex = re.compile('^([0-9a-f]{2})((?: [0-9a-f]{2})+) ([0-9a-f]{2})$')
+
+# Represents a parsed LIN frame
+class LinFrame:
+  def __init__(self, id, data, checksum):
+    self.id = id
+    self.data = data
+    self.checksum = checksum
+ 
+  def __str__(self):
+    return "[" + self.id + "] [" + " ".join(self.data) + "] [" + self.checksum + "]"
+
+  __repr__ = __str__
+
+# Convert a list of two hex digits strings to a list of a single binary digit
+# strings.
+def hexListToBitList(hex_list):
+  result = []
+  for hex_str in hex_list:
+    as_int = int(hex_str, 16)
+    bin_str = bin(as_int)[2:].zfill(8)
+    result.extend(list(bin_str))
+  return result
+
+# If a valid frame return a LinFrame, otherwise 
+# returns None.
+def parseLine(line):
+  m = kFrameRegex.match(line)
+  if not m:
+    return None
+  return LinFrame(m.group(1), m.group(2).split(), m.group(3))
 
 # Parse args and set flags.
 def parseArgs(argv):
-  global flag_speed
-  global flag_port
+  global FLAG_speed
+  global FLAG_port
   try:
-    opts, args = getopt.getopt(argv, "", flag_names)
+    opts, args = getopt.getopt(argv, "", kFlagNames)
   except getopt.GetoptError:
-    print "\n".join(usage)
+    print "\n".join(kUsage)
     sys.exit(2)
   for opt, arg in opts:
     if opt == "--port":
-      flag_port = arg
+      FLAG_port = arg
     elif opt == "--speed":
-      flag_speed = int(arg)
+      FLAG_speed = int(arg)
   print "---"
-  print "Port:  ", flag_port
-  print "Speed: ", flag_speed
+  print "Port:  ", FLAG_port
+  print "Speed: ", FLAG_speed
   print "---"
 
 # Return time now in millis. We use it to comptute relative time.
-def time_millis():
+def timeMillis():
   return int(round(time.time() * 1000))
 
 # Format relative time in millis as "sssss.mmm".
-def format_relative_time_millis(millis):
+def formatRelativeTimeMillis(millis):
   seconds = int(millis / 1000)
   millis_fraction = millis % 1000
   return "%05d.%03d" % (seconds, millis_fraction)
 
 # Clear pending chars until no more.
-def clear_pending_chars(fd):
+def clearPendingChars(fd):
   while (True):
     ready,_,_ = select.select([fd],[],[], 1)
     if not ready:
@@ -68,54 +109,82 @@ def clear_pending_chars(fd):
 
 # Wait for next input character and return it as a single
 # char string.
-def read_char(fd):
+def readChar(fd):
   # Wait for rx data ready. Timeout of 0 indicates wait forever.
   ready,_,_ = select.select([fd],[],[], 0)
   # Read and output one character
   return os.read(fd, 1)
 
-# Read an return a single line, without the eol charcater.
-def read_line(fd):
+# Read and return a single line, without the eol charcater.
+def readLine(fd):
   line = [] 
   while True:
-    char = read_char(fd)
+    char = readChar(fd)
     if (char == "\n"):
       return "".join(line)
     line.append(char)
 
 # Open the serial port for reading at the specified speed.
 # Returns the port's fd.
-def open_port():
+def openPort():
   # Open port in read only mode. Call is blocking.
-  fd = os.open(flag_port, os.O_RDONLY | os.O_NOCTTY )
+  fd = os.open(FLAG_port, os.O_RDONLY | os.O_NOCTTY )
 
   # Setup the port.
   iflag, oflag, cflag, lflag, ispeed, ospeed, cc = termios.tcgetattr(fd) 
-  ispeed = flag_speed
-  ospeed = flag_speed
+  ispeed = FLAG_speed
+  ospeed = FLAG_speed
   termios.tcsetattr(fd, termios.TCSANOW, [iflag, oflag, cflag, lflag, ispeed, ospeed, cc])
-  clear_pending_chars(fd)
+  clearPendingChars(fd)
   return fd
+
+# For now, assuming both are of same size.
+# Return a list of '0', '1' and '-', one per data bit.
+def diffBitLists(old_bit_list, new_bit_list):
+  result = []
+  for idx, new_val in enumerate(new_bit_list):
+    if new_val != old_bit_list[idx]:
+      result.append(new_val)
+    else:
+      result.append("-")
+  return result
+
+# Insert a seperator every n  charaters.
+def insertSeperators(str, n, sep):
+  return sep.join(str[i: i+n] for i in range(0, len(str), n))
 
 def main(argv):
   parseArgs(argv)  
-  fd = open_port()
-  start_time_millis = None
+  fd = openPort()
+  start_time_millis = timeMillis();
+  last_bit_lists = {}
   while True:
-    line = read_line(fd);
-    # First line starts with timestamp of 0.
-    # Note that the timestamp represents the time the end
-    # of the line arrived to this program, not the start
-    # time of the LIN frame. It is used for reference only.
-    if not start_time_millis:
-      start_time_millis = time_millis()
-      rel_time_millis = 0
-    else:
-      rel_time_millis = time_millis() - start_time_millis
-    timestamp = format_relative_time_millis(rel_time_millis);
-    out_line = "%s  %s\n" % (timestamp, line)
-    sys.stdout.write(out_line)
-    sys.stdout.flush()
+    line = readLine(fd);
+    rel_time_millis = timeMillis() - start_time_millis
+    timestamp = formatRelativeTimeMillis(rel_time_millis);
+    if kPrintRawLines:
+      out_line = "%s  %s\n" % (timestamp, line)
+      sys.stdout.write(out_line)
+      sys.stdout.flush()
+    frame = parseLine(line)
+    if not frame:
+      continue
+    id = frame.id
+    new_bit_list = hexListToBitList(frame.data)
+    if id not in last_bit_lists:
+      last_bit_lists[id] = new_bit_list
+      continue
+    old_bit_list = last_bit_lists[id]
+    last_bit_lists[id] = new_bit_list
+    if new_bit_list == old_bit_list:
+      continue
+    if kPrintFrameDiffs:
+      diff_bit_list = diffBitLists(old_bit_list, new_bit_list)
+      diff_str = insertSeperators("".join(diff_bit_list), 4, " ")
+      diff_str = insertSeperators(diff_str, 10, "| ")
+      out_line = "%s  %s: | %s |\n" % (timestamp, id, diff_str)
+      sys.stdout.write(out_line)
+      sys.stdout.flush()
 
 if __name__ == "__main__":
   main(sys.argv[1:])
